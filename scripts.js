db00930731cd05e9dce6ab7736a82ca433d5c12b
Kevin Faria <kevintxd73@gmail.com>
2020-04-07 16:50:28 -0300

    Correção de Bugs
    Foi corrigido o erro de outros algarismos serem inseridos, atrapalhando a correção

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
+
 function converter(){
+    const inputBin = document.querySelector("input#binario")
+    const res = window.document.querySelector('div#res') 
+    const patt1 = /[2-9]/.test(inputBin.value)
+    const label = document.querySelector("label")
+    if ( patt1 == true || inputBin.value.length == 0 ){
+        
+        label.innerHTML = ("DIGITE APENAS 0's ou 1's ")
+        alert("[ERRO] DIGITE APENAS 0's ou 1's ")
+    }else {
+        label.innerHTML = ("Binário")
+        const bin = Number.parseInt(inputBin.value , 2)
+        res.classList.remove("hide")
+        res.innerHTML = (`Decimal = ${bin}`)
 
 
-    var chamar = window.document.getElementById('binario')      
-    var res = window.document.querySelector('div#res')  
-
-    if (chamar.value.length == 0){
-        alert("[ERRO] PREENCHA O CAMPO CORRETAMENTE")
-        res.innerHTML = `[ERRO] PREENCHA O CAMPO CORRETAMENTE` 
-    }else{
-
-    var binario = Number.parseInt(chamar.value , 2)
-    res.innerHTML = `Decimal = ${binario}` 
-
-    res.classList.remove("hide")
-    }    
-
-    
+   }   
 
 }
